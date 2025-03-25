3b3d90e411772570f112c467ff39d5f3d2441401
Your Name <your.email@example.com>
2025-03-25 10:17:32 -0800

    Refactor Pig Latin word processing and remove commented code

--- jacobPim-pigLatin.py
+++ jacobPim-pigLatin.py
@@ -8,37 +8,36 @@ poem = open("Jabberwocky.txt", "r")
 text = poem.read()
 poem.close()
 
-# Step 2) Splitting the line into files
+# splitting lines
 lines = text.split("\n")
 realLines = lines
 
-# Step 3) Splitting the lines into words
 wordLine = []
 wordSum = 0
 
+# remove empty lines
 for line in lines:
     if len(line) == 0:
         lines.remove(line)
 
+# split into words
 for line in lines:
     print(line.split(" "))
     wordLine.append(line.split(" "))
 
+# pigify the words
 for line in range (len(wordLine)):
     for word in range(len(wordLine[line])):
+        thing = wordLine[line][word]
         firstLetter = thing[0]
-        thing = thing[1:] + firstLetter + "ay"
-        print(thing)
+        wordLine[line][word] = thing[1:] + firstLetter + "ay"
 
-"""
+# scrap attempt
 for line in wordLine:
-    for thing in line:
-        firstLetter = thing[0]
-        thing = thing[1:] + firstLetter + "ay"
-        print(thing)
-"""
+    currentLine = " ".join(line)
+    print(currentLine)
 
-print(wordLine)
+# print(wordLine)
         
 
 # Step 4) Creating Pig Latin lines
